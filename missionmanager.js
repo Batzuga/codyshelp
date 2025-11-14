25c9f13c4892dc06cde4d3b30f44ae146c4ff512
Batzuga <jere.ahvenus@gmail.com>
2025-11-14 09:15:17 +0200

    hint text fixes

--- missionmanager.js
+++ missionmanager.js
@@ -3,44 +3,44 @@ var hints = [
     {
         id: 0,
         name: "Tutorial",
-        hint1: "No coding is required.",
-        hint2: "Find the Player in hierarchy.",
-        hint3: "Check the movement speed variable in the Player component on Player object."
+        hint1: "Hint 1 - No coding is required.",
+        hint2: "Hint 2 - Find the Player in hierarchy.",
+        hint3: "Hint 3 - Check the movement speed variable in the Player component on Player object."
     },
     {
         id: 1,
         name: "Mission 1 - Light's out",
-        hint1: "Light source",
-        hint2: "Check the Global Light 2D.",
-        hint3: "Light intensity should be at 1."
+        hint1: "Hint 1 - Light source",
+        hint2: "Hint 2 - Check the Global Light 2D.",
+        hint3: "Hint 3 - Light intensity should be at 1."
     },  
     {
         id: 2,
         name: "Mission 2 - 20/20",
-        hint1: "Find the SignPost object in hierarchy. (Very, VERY, tiny in scene)",
-        hint2: "Check the texture settings, and keep the object scale at (1, 1, 1)",
-        hint3: "Pixels Per Unit, Filter Mode, Compression"
+        hint1: "Hint 1 - Find the SignPost object in hierarchy. (Very, VERY, tiny in scene)",
+        hint2: "Hint 2 - Check the texture settings, and keep the object scale at (1, 1, 1)",
+        hint3: "Hint 3 - Pixels Per Unit, Filter Mode, Compression"
     },  
     {
         id: 3,
         name: "Mission 3 - It's in the name",
-        hint1: "Navigate to the place where you change project name",
-        hint2: "Project settings",
-        hint3: "Product name"
+        hint1: "Hint 1 - Navigate to the place where you change project name",
+        hint2: "Hint 2 - Project settings",
+        hint3: "Hint 3 - Product name"
     },  
     {
         id: 4,
         name: "Mission 4 - Quiz Master",
-        hint1: "This one you have to do on your own.",
-        hint2: "You'll learn from wrong answers too.",
-        hint3: "You can repeat this until you know at least half of the answers by heart!"
+        hint1: "Hint 1 - This one you have to do on your own.",
+        hint2: "Hint 2 - You'll learn from wrong answers too.",
+        hint3: "Hint 3 - You can repeat this until you know at least half of the answers by heart!"
     },  
     {
         id: 5,
         name: "Mission 5 - More Ground",
-        hint1: "Tile Palette",
-        hint2: "Open a tile palette window select the grass tile and draw more ground.",
-        hint3: "Make sure you are on the correct layer on the Tile Palette"
+        hint1: "Hint 1 - Tile Palette",
+        hint2: "Hint 2 - Open a tile palette window select the grass tile and draw more ground.",
+        hint3: "Hint 3 - Make sure you are on the correct layer on the Tile Palette"
     },  
     {
         id: 123,
